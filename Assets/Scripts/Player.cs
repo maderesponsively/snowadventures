32d642626682a4b3c2053f7dc874eb77d24006d2
David Smith <hello@maderesponsively.com>
2020-06-20 22:33:09 +0100

    Integrating snow pick up

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+
+    public float runSpeed = 40f;
+    private float _horizontalMove = 0f;
+    private bool _jump = false;
+
+    [SerializeField]
+    private int _snowCollected = 0;
+    [SerializeField]
+    private int _snowCollectedMax = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        _horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(_horizontalMove));
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            _jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+    }
+
+    public void OnLanding ()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
+    void FixedUpdate()
+    {
+        // Move our Character
+        controller.Move(_horizontalMove * Time.fixedDeltaTime, false, _jump);
+        _jump = false;
+    }
+
+    public void CollectSnow()
+    {
+        if(_snowCollected <= _snowCollectedMax)
+        {
+            animator.SetBool("IsCollecting", true);
+        }
+    }
+
+    public void CollectedSnow()
+    {
+        _snowCollected++;
+        animator.SetBool("IsCollecting", false);
+    }
+
+    public void StopCollectingSnow()
+    {
+        animator.SetBool("IsCollecting", false);
+    }
+}
---
+++ Assets/Scripts/Snow.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snow : MonoBehaviour
+{
+    [SerializeField]
+    private float _waitTime = 1.4f;
+
+    private bool _collectSnow = false;
+    private IEnumerator coroutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Player player = collision.transform.GetComponent<Player>();
+
+            if (player != null)
+            {
+                if (_collectSnow == false)
+                {
+                    _collectSnow = true;
+
+                    player.CollectSnow();
+
+                    coroutine = SnowCollectionCoroutine(player);
+
+                    StartCoroutine(coroutine);
+                }
+
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Player player = collision.transform.GetComponent<Player>();
+
+            if (player != null)
+            {
+                _collectSnow = false;
+
+                player.StopCollectingSnow();
+
+                StopCoroutine(coroutine);
+            }
+        }
+    }
+
+    IEnumerator SnowCollectionCoroutine(Player player)
+    {
+        while(_collectSnow == true)
+        {
+            yield return new WaitForSeconds(_waitTime);
+
+            player.CollectedSnow();
+
+            Destroy(this.gameObject);
+        }
+    }
+}
